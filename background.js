34704dd0b5687e77f2c2c450909300dee23b58f6
PickolZi <pickol876@gmail.com>
2024-07-07 16:14:39 -0700

    Initial commit

---
+++ background.js
@@ -0,0 +1,12 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.action.setBadgeText({
+    text: "OFF",
+  });
+});
+
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ["content.js"],
+  });
+});
