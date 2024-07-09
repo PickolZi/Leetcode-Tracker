ccf499e75ea262582e9be5303076e1230c2edf49
PickolZi <pickol876@gmail.com>
2024-07-08 23:47:04 -0700

    Finally successfully able to manipulate the DOM through the content.js script and observers. Next need to get all the data like submissions, title, tags, and more

--- content.js
+++ content.js
@@ -1,1 +1,21 @@
-alert("Hello, world!");
+function handleMutations(mutationsList, observer) {
+  const leetcodeHeader = document.querySelector("div.text-title-large a");
+
+  if (leetcodeHeader) {
+    console.log("Leetcode header found: ", leetcodeHeader);
+
+    const header = leetcodeHeader.textContent;
+    console.log("Header is: ", header);
+
+    observer.disconnect();
+  }
+}
+
+const observer = new MutationObserver(handleMutations);
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+});
+
+handleMutations();
